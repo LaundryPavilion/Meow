40670650fa773b01f1f9a64eed2001a182db5fa1
Craig <m2xcomcastnet@gmail.com>
2012-12-05 09:10:48 -0700

    Adding first VS project to git

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿using System.Windows.Forms;
+namespace object_to_nullable_nit_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) Application.RemoveMessageFilter(this);
+
+            if (disposing && (components != null))
+            {
+
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btn_ReaderMode = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(599, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(376, 196);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseDown);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 246);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.richTextBox1.Size = new System.Drawing.Size(281, 164);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // btn_ReaderMode
+            // 
+            this.btn_ReaderMode.Location = new System.Drawing.Point(394, 21);
+            this.btn_ReaderMode.Name = "btn_ReaderMode";
+            this.btn_ReaderMode.Size = new System.Drawing.Size(75, 23);
+            this.btn_ReaderMode.TabIndex = 3;
+            this.btn_ReaderMode.Text = "Reader mode";
+            this.btn_ReaderMode.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2500;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Location = new System.Drawing.Point(417, 246);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(61, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(300, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 447);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_ReaderMode);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn_ReaderMode;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace object_to_nullable_nit_test
+{
+    public partial class Form1 : Form, IMessageFilter 
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            Application.AddMessageFilter(this);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+                panel1.Controls.Add(new TextBox());
+
+            string nullStr = null;
+
+            string test = string.Format("this ia a test {0}", nullStr);
+
+            object one = "asdasd";
+            object two = "1212";
+            object three = 123;
+            object four = 123L;
+            object five = 123.4;
+
+            var oneResult = one as int?;
+            var twoResult = two as int?;
+            var threeResult = three as int?;
+            var fourResult = four as int?;
+            var fiveResult = five as int?;
+
+            var oneResultB = one as long?;
+            var twoResultB = two as long?;
+            var threeResultB = three as long?;
+            var fourResultB = four as long?;
+            var fiveResultB = five as long?;
+
+
+        }
+
+        public void DisplayInvalidChars()
+        {
+            // Get a list of invalid path characters. 
+            string[] chars = (from char c in Path.GetInvalidPathChars() where c != '\0' select c.ToString()).ToArray();
+            this.textBox1.Text = string.Join(Environment.NewLine, chars);
+
+            chars = (from char c in Path.GetInvalidFileNameChars() where c != '\0' select c.ToString()).ToArray();
+            this.richTextBox1.Text = string.Join(Environment.NewLine, chars);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            cts.Token.ThrowIfCancellationRequested();
+            var task = Task.Factory.StartNew(() =>  
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        cts.Token.ThrowIfCancellationRequested();
+                        Thread.Sleep(500);
+                    }
+
+                    // Won't get here if cancelled.
+                    MessageBox.Show("was not cancelled");
+                }, cts.Token);
+
+               cts.Token.Register(() => {MessageBox.Show("Cancelled!");});
+
+            // Wait for Task to startup
+            while (task.Status != TaskStatus.Running) { };
+
+            try
+            {
+                cts.Cancel();
+                task.Wait();
+            }
+            catch (AggregateException aex)
+            {
+                aex.Handle(x => { return true; });
+            }
+        }
+
+        public bool PreFilterMessage(ref Message m) {
+            if (m.HWnd == this.panel1.Handle) {
+                if (m.Msg == WM_MBUTTONDOWN)
+                {
+                    EnterReaderMode();
+                }
+            }
+            return false;
+        }
+
+        public const int WM_MBUTTONDOWN = 0x0207;
+
+        [DllImport("comctl32.dll", SetLastError=true,  EntryPoint="#383")]
+        static extern void DoReaderMode(ref READERMODEINFO prmi);
+
+        public delegate bool TranslateDispatchCallbackDelegate(ref MSG lpmsg);
+        public delegate bool ReaderScrollCallbackDelegate(ref READERMODEINFO prmi, int dx, int dy);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct READERMODEINFO
+        {
+            public int cbSize;
+            public IntPtr hwnd;
+            public int fFlags;
+            public IntPtr prc;
+            public ReaderScrollCallbackDelegate pfnScroll;
+            public TranslateDispatchCallbackDelegate fFlags2;
+            public IntPtr lParam;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MSG
+        {
+            public IntPtr hwnd;
+            public UInt32 message;
+            public IntPtr wParam;
+            public IntPtr lParam;
+            public UInt32 time;
+            public POINT pt;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct RECT
+        {
+            public int left, top, right, bottom;
+        }
+
+        private bool TranslateDispatchCallback(ref MSG lpMsg)
+        {
+            return false;
+        }
+        private bool ReaderScrollCallback(ref READERMODEINFO prmi, int dx, int dy)
+        {
+            // TODO: Scroll around within your control here
+
+            return false;
+        }
+
+        private void EnterReaderMode()
+        {
+            READERMODEINFO readerInfo = new READERMODEINFO
+            {
+                hwnd = this.textBox1.Handle,
+                fFlags = 0,
+                prc = IntPtr.Zero,
+                lParam = IntPtr.Zero,
+                fFlags2 = new TranslateDispatchCallbackDelegate(this.TranslateDispatchCallback),
+                pfnScroll = new ReaderScrollCallbackDelegate(this.ReaderScrollCallback)
+            };
+            readerInfo.cbSize = Marshal.SizeOf(readerInfo);
+
+            DoReaderMode(ref readerInfo);
+        }
+
+        private void textBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Middle)
+            {
+                EnterReaderMode();
+            }
+        }
+    }
+}
